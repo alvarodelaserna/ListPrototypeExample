da9ca4ee45d5f91a16483a711474ca89f570665f
alvaro.delaserna <alvaro.delaserna@bbva.com>
2015-12-28 14:03:23 +0100

    Add Ionic version

--- Ionic/listPrototypeExample/www/js/app.js
+++ Ionic/listPrototypeExample/www/js/app.js
@@ -5,12 +5,6 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
-.config(['$httpProvider', function($httpProvider) {
-    $httpProvider.defaults.useXDomain = true;
-    // $httpProvider.defaults.withCredentials = true;
-    delete $httpProvider.defaults.headers.common['X-Requested-With'];
-  }])
-
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
